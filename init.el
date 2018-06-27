476365a0643513ecda68b3ef5aef0cca1f11d4b7
Javier Coppis <jcoppis@grupoassa.com>
2018-06-27 17:31:54 -0300

    agregado flycheck

--- init.el
+++ init.el
@@ -75,7 +75,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(js-indent-level 2)
- '(package-selected-packages (quote (zenburn-theme js2-mode magit alchemist counsel))))
+ '(package-selected-packages
+   (quote
+    (flycheck zenburn-theme js2-mode magit alchemist counsel))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
